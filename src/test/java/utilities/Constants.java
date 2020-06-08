9b0bb2fd1ac9ce925ce4d0c37997ba866b388437
Lerry Ortiz <lerryortiz@MacBook-Pro-de-Lerry.local>
2020-06-07 21:44:37 -0400

    refactor for running tests either on mac or linux

--- src/test/java/utilities/Constants.java
+++ src/test/java/utilities/Constants.java
@@ -4,7 +4,8 @@ public class Constants {
 
     public final static String BASE_URL = "https://google-gruyere.appspot.com/625965704642844904637778629627729382289/";
     public final static String EXPECTED_HOME_TITLE = "Gruyere: Home";
-    public final static String CHROMEDRIVER_EXECUTABLE_PATH = System.getProperty("user.dir")+"/src/test/resources/chromedriver";
+    public final static String CHROMEDRIVER_MAC_EXECUTABLE_PATH = System.getProperty("user.dir")+"/src/test/resources/chromedriver_mac/chromedriver";
+    public final static String CHROMEDRIVER_LINUX_EXECUTABLE_PATH = System.getProperty("user.dir")+"/src/test/resources/chromedriver_linux/chromedriver";
     public final static String EXPECTED_SIGN_UP_TITLE = "Sign up for a new account.";
     public final static String EXPECTED_SIGN_IN_TITLE = "Gruyere: Login";
     public final static String EXPECTED_NEW_SNIPPED_TITLE = "Gruyere: New Snippet";
--- src/test/java/utilities/Hooks.java
+++ src/test/java/utilities/Hooks.java
@@ -12,8 +12,18 @@ public class Hooks {
     protected WebDriver driver;
 
     @Before
-    public void initDriver(){
-        System.setProperty("webdriver.chrome.driver", Constants.CHROMEDRIVER_EXECUTABLE_PATH);
+    public void initDriver() throws Exception {
+        switch (System.getProperty("os").toLowerCase()) {
+            case "linux":
+                System.setProperty("webdriver.chrome.driver", Constants.CHROMEDRIVER_LINUX_EXECUTABLE_PATH);
+                break;
+            case "osx":
+                System.setProperty("webdriver.chrome.driver", Constants.CHROMEDRIVER_MAC_EXECUTABLE_PATH);
+                break;
+            default:
+                throw new Exception("No OS specified, please run test with the following format specifying whether your system is " +
+                                    "linux or osx: './gradlew test -Dos=your_OS_here'");
+        }
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
